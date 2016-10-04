207be0b58950dfb4507f920538bb0ed238d8b68e
ZenBarrier <anthony.j.barrera@gmail.com>
2016-10-03 23:10:26 -0400

    main class skeleton

--- app/src/main/java/com/zenbarrier/memorableplaces/MainActivity.java
+++ app/src/main/java/com/zenbarrier/memorableplaces/MainActivity.java
@@ -1,13 +1,42 @@
 package com.zenbarrier.memorableplaces;
 
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Geocoder geocoder;
+    ArrayList<String> places;
+    ListView listView;
+
+    public void addPlace(View view){
+        
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
     }
 }
